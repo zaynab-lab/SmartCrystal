6cc7ccdde4f01356a5608d2909bb973fff471fdf
Zaynab <75333513+zaynab-lab@users.noreply.github.com>
2021-10-30 12:43:00 +0300

    has sw ??

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ module.exports = withPWA({
   reactStrictMode: true,
   pwa: {
     dest: "public",
-    disable: process.env.NODE_ENV === "development",
     register: true,
     skipWaiting: true
   }
