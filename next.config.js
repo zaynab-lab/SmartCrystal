314c2a323cfc0dda39826c61621bb5ebe1bef4a1
Zaynab <75333513+zaynab-lab@users.noreply.github.com>
2021-10-30 12:36:57 +0300

    prelast test pwa

--- next.config.js
+++ next.config.js
@@ -4,23 +4,8 @@ module.exports = withPWA({
   reactStrictMode: true,
   pwa: {
     dest: "public",
+    disable: process.env.NODE_ENV === "development",
     register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development"
+    skipWaiting: true
   }
 });
-
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader",
-        options: {
-          // eslint options (if necessary)
-        }
-      }
-    ]
-  }
-};
